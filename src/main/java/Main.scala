52ed7127bfd9f41441dc578a6c779459459f27e9
Bartosz Skotarek <bzskotarek@gmail.com>
2016-11-12 18:16:23 +0100

    Zadanie 7

--- src/main/java/Main.scala
+++ src/main/java/Main.scala
@@ -2,12 +2,13 @@ import java.util
 import java.util.Properties
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.{Dataset, SaveMode, SparkSession}
+import org.apache.spark.sql.{Dataset, Row, SaveMode, SparkSession}
 import pl.sgjp.morfeusz.{Morfeusz, MorfeuszUsage, MorphInterpretation}
 
+import scala.collection.mutable.ArrayBuffer
 import scala.collection.{JavaConversions, mutable}
 
-case class Advertisement(id: Long, title: String)
+case class Advertisement(id: Long, title: String, category1: Long, category2: Option[Long], category3: Option[Long])
 
 case class BagOfWords(advertisementId: Long, word: String)
 
@@ -47,23 +48,98 @@ object Main extends java.io.Serializable {
         //
         val advertisementDS = trainingRDD.map(string => string.split("\t"))
                 .map(
-                    stringArray => Advertisement(stringArray(0).toLong, stringArray(1))
+                    stringArray => Advertisement(
+                        stringArray(0).toLong, stringArray(1),
+                        stringArray(4).toLong,
+                        if (!stringArray(5).isEmpty) Option(stringArray(5).toLong) else None,
+                        if (stringArray.length == 7) Option(stringArray(6).toLong) else None
+                    )
                 )
                 .toDS()
         //
         bagOfWordsDS.createOrReplaceTempView("bag_of_words")
         advertisementDS.createOrReplaceTempView("advertisement")
 
-        // 5 zadanie
-        val liczbaCech = sqlContext.sql("SELECT COUNT(1) AS LiczbaCech FROM (SELECT DISTINCT word FROM bag_of_words)")
+        // odpowiedzi
+        exercise5()
+        exercise7()
+
+        //        saveInDatabase(bagOfWordsDS, advertisementDS)
+    }
+
+    def exercise5(): Unit = {
+        println("Zadanie 5")
+
+        val numberOfFeatures = sqlContext.sql("SELECT COUNT(1) AS LiczbaCech FROM (SELECT DISTINCT word FROM bag_of_words)")
                 .collect()(0).getLong(0)
-        println(s"Liczba cech: $liczbaCech")
+        println(s"Liczba cech: $numberOfFeatures")
 
-        val gestosc = sqlContext.sql(s"SELECT AVG(gestosc) FROM (SELECT advertisementId, COUNT(1)/$liczbaCech AS gestosc FROM bag_of_words GROUP BY advertisementId)")
+        val density = sqlContext.sql(s"SELECT AVG(gestosc) FROM (SELECT advertisementId, COUNT(1)/$numberOfFeatures AS gestosc FROM bag_of_words GROUP BY advertisementId)")
                 .collect()(0).getDouble(0)
-        println(f"Gestosc zbioru: $gestosc%.6f")
+        println(f"Gestosc zbioru: $density%.6f")
+    }
 
-//        saveInDatabase(bagOfWordsDS, advertisementDS)
+    def exercise7(): Unit = {
+        println("Zadanie 7")
+
+        // pierwsze zdanie
+        sqlContext.sql("SELECT word, COUNT(word) AS LiczbaOgloszen FROM bag_of_words GROUP BY word ORDER BY COUNT(word) DESC").show(50)
+
+        // drugie zdanie
+        val features = sqlContext.sql("SELECT word, COUNT(word) AS LiczbaOgloszen FROM bag_of_words GROUP BY word ORDER BY COUNT(word) DESC LIMIT 50")
+        features.foreach(row => {
+            val feature = row.getString(0)
+            val numberOfAdvertisements = row.getString(0)
+
+            val advertisementsDS = sqlContext.sql(s"SELECT category1, category2, category3 FROM bag_of_words JOIN advertisement ON advertisementId=id WHERE word='$feature'")
+
+            val categoryCounter: CategoryCounter = new CategoryCounter()
+            val advertisements: Array[Row] = advertisementsDS.collect()
+            categoryCounter.add(advertisements)
+
+            val theMostFrequentCategory = categoryCounter.getTheMostFrequentCategory
+            val categoryId = theMostFrequentCategory._1
+            val counter = theMostFrequentCategory._2
+
+            val frequency = counter / advertisements.length.toDouble
+
+            println(f"Slowo: $feature | najczesciej wystepujaca kategoria: $categoryId | liczba wystapien $counter | czestotliwosc wystepowania kategorii: $frequency%.6f")
+        })
+    }
+
+    class CategoryCounter {
+        val map: mutable.Map[Long, Long] = mutable.Map()
+
+        def add(category: Array[Row]): Unit = {
+            category.foreach((row: Row) => {
+                val categories = ArrayBuffer[Long]()
+                if (!row.isNullAt(0)) categories += row.getLong(0)
+                if (!row.isNullAt(1)) categories += row.getLong(1)
+                if (!row.isNullAt(2)) categories += row.getLong(2)
+
+                categories.foreach((category: Long) => {
+                    val counter: Long = map.getOrElse(category, 0L)
+                    val newCounter = counter + 1
+                    map(category) = newCounter
+                })
+            })
+        }
+
+        def getTheMostFrequentCategory: (Long, Long) = {
+            var theMostFrequentItem: (Long, Long) = null
+
+            map.foreach((tuple: (Long, Long)) => {
+                if (theMostFrequentItem == null) {
+                    theMostFrequentItem = tuple
+                } else {
+                    if (tuple._2 > theMostFrequentItem._2) {
+                        theMostFrequentItem = tuple
+                    }
+                }
+            })
+
+            theMostFrequentItem
+        }
     }
 
     // znaczenie http://nkjp.pl/poliqarp/help/plse2.html
@@ -92,7 +168,9 @@ object Main extends java.io.Serializable {
 
         val withoutStopWords = withoutTags.filter(res => !stopWords.contains(res.getOrth))
 
-        getFirstLemmaFromEveryNode(withoutStopWords)
+        val lemmas = getFirstLemmaFromEveryNode(withoutStopWords)
+
+        lemmas.distinct // usuniecie duplikatow - w tytule pojawiaja sie dwa razy te same slowo
     }
 
     def removeByTags(result: util.List[MorphInterpretation]): mutable.Buffer[MorphInterpretation] = {
